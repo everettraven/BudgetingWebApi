aefe2c8e1fb62e4ca6ff90f9fed7b7731265e162
everettraven <everettraven@gmail.com>
2019-09-13 21:36:43 -0400

    finished creating test for current API endpoints

--- README.md
+++ README.md
@@ -7,23 +7,21 @@ This is a REST Web Api developed by Bryce Palmer using Python and the Flask micr
 4. Learn the basics of Docker
 5. Learn proper Web API security and standards
 
+## API Endpoints
 
-
-
-## Documentation & Code to work on:
     1. APINAME/user/auth - generate an access token and refresh token <-- Coded  <-- Tests Created
 
     2. APINAME/user/categories & APINAME/user/categories/id - GET all/specific for the current user <-- Coded <-- Tests Created
 
     3. APINAME/user/categories/add - POST to add a new category for a user <-- Coded <-- Test Created
 
-    4. APINAME/user/categories/id/update - POST to update a category for a user <-- Coded
+    4. APINAME/user/categories/id/update - POST to update a category for a user <-- Coded <-- Test Created
 
-    5. APINAME/user/categories/id/expenses - GET to get all the expenses from a specific category <-- Coded
+    5. APINAME/user/categories/id/expenses - GET to get all the expenses from a specific category <-- Coded <-- Test Created
 
-    6. APINAME/user/categories/id/expenses/add  - POST to add a new expense for a category <-- Coded
+    6. APINAME/user/categories/id/expenses/add  - POST to add a new expense for a category <-- Coded <-- Test Created
 
-    7. APINAME/user - Accept GET and POST to get and modify information on a user <-- Coded
+    7. APINAME/user - Accept GET and POST to get and modify information on a user <-- Coded <-- Tests Created
     
     8. APINAME/user/register - Registers a new user <-- Coded <-- Test Created
 
@@ -31,7 +29,7 @@ This is a REST Web Api developed by Bryce Palmer using Python and the Flask micr
 
     10. APINAME/user/login - Login on behalf of a user <-- Coded <-- Test Created
 
-    11. APINAME/user/categories/id/expenses/id/update <-- Coded
+    11. APINAME/user/categories/id/expenses/id/update <-- Coded <-- Test Created
 
-## Todo
-- [ ] Create Unit Tests
+    12. APINAME/cleanup_tests <-- Coded <-- Used at the end of the test file.
+
--- tests.py
+++ tests.py
@@ -233,9 +233,266 @@ class API_Tests(unittest.TestCase):
         self.assertEqual(cat_resp["name"], "Gas1", "The name of the category with the specified id should be Gas1")
         self.assertEqual(cat_resp["id"], id_to_get, "The id of the item gotten should be " + str(id_to_get))
 
+    def test_categories_update(self):
+        auth_code = ""
+        access_token = ""
+        id_to_get = 0;
+        
+        # Register a new user
+        data = {"username": "CatUpdateTester", "password": "CatUpdateTester"}
+        headers = {"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+        reg_r = requests.post(self.base_url + "/user/register", json=data, headers=headers)
+
+        # Login as new user
+        login_r = requests.post(self.base_url + "/user/login", json=data, headers=headers)
+        login_resp = login_r.json()
+        auth_code = login_resp["authorization_code"]
+
+        # Exchange auth_code for access_token and refresh_token
+        auth_data = {"code": auth_code}
+        auth_r = requests.post(self.base_url + "/user/auth", json=auth_data, headers=headers)
+        auth_resp = auth_r.json()
+
+        access_token = auth_resp["access_token"]
+
+        for i in range(3):
+            cat_data = {"name": "Gas" + str(i)}
+            cat_headers = {"Authorization": "Bearer " + str(access_token) ,"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+            cat_r = requests.post(self.base_url + "/user/categories/add", json=cat_data, headers=cat_headers)
+            if i == 1:
+                cat_resp = cat_r.json()
+                id_to_get = cat_resp["categories"][1]["id"]
+            
+        update_data = {"name": "Updated"}
+        update_headers = {"Authorization": "Bearer " + str(access_token) ,"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+        update_r = requests.post(self.base_url + "/user/categories/" + str(id_to_get) + "/update", json=update_data, headers=update_headers)
+        update_resp = update_r.json()
+
+        self.assertEqual(update_resp["name"], "Updated", "The name of the category updated should be Updated")
+        self.assertEqual(update_resp["id"], id_to_get, "The id of the item updated should be " + str(id_to_get))
+
+    def test_expenses_add(self):
+        auth_code = ""
+        access_token = ""
+        
+        # Register a new user
+        data = {"username": "ExpAddTester", "password": "ExpAddTester"}
+        headers = {"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+        reg_r = requests.post(self.base_url + "/user/register", json=data, headers=headers)
+
+        # Login as new user
+        login_r = requests.post(self.base_url + "/user/login", json=data, headers=headers)
+        login_resp = login_r.json()
+        auth_code = login_resp["authorization_code"]
+
+        # Exchange auth_code for access_token and refresh_token
+        auth_data = {"code": auth_code}
+        auth_r = requests.post(self.base_url + "/user/auth", json=auth_data, headers=headers)
+        auth_resp = auth_r.json()
+
+        access_token = auth_resp["access_token"]
+
+        cat_data = {"name": "Gas"}
+        cat_headers = {"Authorization": "Bearer " + str(access_token) ,"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+        cat_r = requests.post(self.base_url + "/user/categories/add", json=cat_data, headers=cat_headers)
+        cat_resp = cat_r.json()
+        id_to_use = cat_resp["categories"][0]["id"]
+
+        exp_data = {"description": "Got gas", "amount": 38.76, "location": "Circle-K"}
+        exp_r = requests.post(self.base_url + "/user/categories/" + str(id_to_use) + "/expenses/add", json=exp_data, headers=cat_headers)
+        exp_resp = exp_r.json()
+
+        self.assertEqual(exp_resp["categories"][0]["expenses"][0]["description"], "Got gas", "The description should be Got gas")
+        self.assertTrue(len(exp_resp["categories"][0]["expenses"]) == 1, "The length of expenses should be 1")
+        self.assertEqual(exp_resp["spent"], 38.76, "The user's spent amount should be 38.76")
+        self.assertEqual(exp_resp["categories"][0]["expenses"][0]["amount"], 38.76, "amount should be 38.76")
+        self.assertEqual(exp_resp["categories"][0]["expenses"][0]["location"], "Circle-K", "The location should be Circle-K")
+        self.assertTrue(exp_resp["categories"][0]["expenses"][0]["date"] is not None, "The date field of the JSON response should not be NoneType")
+    
+    def test_get_expenses(self):
+        auth_code = ""
+        access_token = ""
+        
+        # Register a new user
+        data = {"username": "ExpGetTester", "password": "ExpGetTester"}
+        headers = {"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+        reg_r = requests.post(self.base_url + "/user/register", json=data, headers=headers)
+
+        # Login as new user
+        login_r = requests.post(self.base_url + "/user/login", json=data, headers=headers)
+        login_resp = login_r.json()
+        auth_code = login_resp["authorization_code"]
+
+        # Exchange auth_code for access_token and refresh_token
+        auth_data = {"code": auth_code}
+        auth_r = requests.post(self.base_url + "/user/auth", json=auth_data, headers=headers)
+        auth_resp = auth_r.json()
+
+        access_token = auth_resp["access_token"]
+
+        cat_data = {"name": "Gas"}
+        cat_headers = {"Authorization": "Bearer " + str(access_token) ,"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+        cat_r = requests.post(self.base_url + "/user/categories/add", json=cat_data, headers=cat_headers)
+        cat_resp = cat_r.json()
+        id_to_use = cat_resp["categories"][0]["id"]
+
+        exp_data = {"description": "Got gas", "amount": 38.76, "location": "Circle-K"}
+        exp_r = requests.post(self.base_url + "/user/categories/" + str(id_to_use) + "/expenses/add", json=exp_data, headers=cat_headers)
+        exp_resp = exp_r.json()
+
+        get_headers = {"Authorization": "Bearer " + str(access_token) ,"client_id": self.client_id, "client_secret": self.client_secret}
+        get_r = requests.get(self.base_url + "/user/categories/" + str(id_to_use) + "/expenses", headers=get_headers)
+        get_resp = get_r.json()
+
+        self.assertTrue(len(get_resp["expenses"]) == 1, "There should only be 1 expense returned")
+    
+    def test_get_expenses_mult(self):
+        auth_code = ""
+        access_token = ""
+        
+        # Register a new user
+        data = {"username": "ExpGetMultTester", "password": "ExpGetMultTester"}
+        headers = {"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+        reg_r = requests.post(self.base_url + "/user/register", json=data, headers=headers)
+
+        # Login as new user
+        login_r = requests.post(self.base_url + "/user/login", json=data, headers=headers)
+        login_resp = login_r.json()
+        auth_code = login_resp["authorization_code"]
+
+        # Exchange auth_code for access_token and refresh_token
+        auth_data = {"code": auth_code}
+        auth_r = requests.post(self.base_url + "/user/auth", json=auth_data, headers=headers)
+        auth_resp = auth_r.json()
+
+        access_token = auth_resp["access_token"]
+
+        cat_data = {"name": "Gas"}
+        cat_headers = {"Authorization": "Bearer " + str(access_token) ,"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+        cat_r = requests.post(self.base_url + "/user/categories/add", json=cat_data, headers=cat_headers)
+        cat_resp = cat_r.json()
+        id_to_use = cat_resp["categories"][0]["id"]
+        for i in range(5):
+            
+            exp_data = {"description": "Got gas" + str(i), "amount": 38.76 + i, "location": "Circle-K"}
+            exp_r = requests.post(self.base_url + "/user/categories/" + str(id_to_use) + "/expenses/add", json=exp_data, headers=cat_headers)
+            exp_resp = exp_r.json()
+
+        get_headers = {"Authorization": "Bearer " + str(access_token) ,"client_id": self.client_id, "client_secret": self.client_secret}
+        get_r = requests.get(self.base_url + "/user/categories/" + str(id_to_use) + "/expenses", headers=get_headers)
+        get_resp = get_r.json()
+
+        self.assertTrue(len(get_resp["expenses"]) == 5, "There should only be 5 expenses returned")
+    
+    def test_expenses_update(self):
+        auth_code = ""
+        access_token = ""
+        
+        # Register a new user
+        data = {"username": "ExpUpdateTester", "password": "ExpUpdateTester"}
+        headers = {"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+        reg_r = requests.post(self.base_url + "/user/register", json=data, headers=headers)
+
+        # Login as new user
+        login_r = requests.post(self.base_url + "/user/login", json=data, headers=headers)
+        login_resp = login_r.json()
+        auth_code = login_resp["authorization_code"]
+
+        # Exchange auth_code for access_token and refresh_token
+        auth_data = {"code": auth_code}
+        auth_r = requests.post(self.base_url + "/user/auth", json=auth_data, headers=headers)
+        auth_resp = auth_r.json()
+
+        access_token = auth_resp["access_token"]
+
+        cat_data = {"name": "Gas"}
+        cat_headers = {"Authorization": "Bearer " + str(access_token) ,"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+        cat_r = requests.post(self.base_url + "/user/categories/add", json=cat_data, headers=cat_headers)
+        cat_resp = cat_r.json()
+        id_to_use = cat_resp["categories"][0]["id"]
+
+        exp_data = {"description": "Got gas", "amount": 38.76, "location": "Circle-K"}
+        exp_r = requests.post(self.base_url + "/user/categories/" + str(id_to_use) + "/expenses/add", json=exp_data, headers=cat_headers)
+        exp_resp = exp_r.json()
+        exp_id = exp_resp["categories"][0]["expenses"][0]["id"]
+            
+        update_data = {"description": "Got a Slurpee and Gas", "amount": 34.67, "location": "7Eleven"}
+        update_headers = {"Authorization": "Bearer " + str(access_token) ,"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+        update_r = requests.post(self.base_url + "/user/categories/expenses/" + str(exp_id) + "/update", json=update_data, headers=update_headers)
+        update_resp = update_r.json()
+
+        self.assertEqual(update_resp["categories"][0]["expenses"][0]["description"], "Got a Slurpee and Gas", "The description should be Got a Slurpee and Gas")
+        self.assertTrue(len(update_resp["categories"][0]["expenses"]) == 1, "The length of expenses should be 1")
+        self.assertEqual(update_resp["spent"], 34.67, "The user's spent amount should be 34.67")
+        self.assertEqual(update_resp["categories"][0]["expenses"][0]["amount"], 34.67, "amount should be 38.76")
+        self.assertEqual(update_resp["categories"][0]["expenses"][0]["location"], "7Eleven", "The location should be 7Eleven")
+        self.assertTrue(update_resp["categories"][0]["expenses"][0]["date"] is not None, "The date field of the JSON response should not be NoneType")
+    
+    def test_user_post(self):
+        auth_code = ""
+        access_token = ""
+        
+        # Register a new user
+        data = {"username": "UserPostTester", "password": "UserPostTester"}
+        headers = {"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+        reg_r = requests.post(self.base_url + "/user/register", json=data, headers=headers)
+
+        # Login as new user
+        login_r = requests.post(self.base_url + "/user/login", json=data, headers=headers)
+        login_resp = login_r.json()
+        auth_code = login_resp["authorization_code"]
+
+        # Exchange auth_code for access_token and refresh_token
+        auth_data = {"code": auth_code}
+        auth_r = requests.post(self.base_url + "/user/auth", json=auth_data, headers=headers)
+        auth_resp = auth_r.json()
+
+        access_token = auth_resp["access_token"]
+
+        post_data = {"spendingLimit": 300.89}
+        post_headers = {"Authorization": "Bearer " + str(access_token) ,"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+        post_r = requests.post(self.base_url + "/user", json=post_data, headers=post_headers)
+        post_resp = post_r.json()
+
+        self.assertEqual(post_resp["spendingLimit"], 300.89, "Spending limit should be 300.89")
+        self.assertEqual(post_resp["username"], "UserPostTester", "Name should be UserPostTester")
+
+    def test_user_get(self):
+        auth_code = ""
+        access_token = ""
+        
+        # Register a new user
+        data = {"username": "UserGetTester", "password": "UserGetTester"}
+        headers = {"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+        reg_r = requests.post(self.base_url + "/user/register", json=data, headers=headers)
+
+        # Login as new user
+        login_r = requests.post(self.base_url + "/user/login", json=data, headers=headers)
+        login_resp = login_r.json()
+        auth_code = login_resp["authorization_code"]
+
+        # Exchange auth_code for access_token and refresh_token
+        auth_data = {"code": auth_code}
+        auth_r = requests.post(self.base_url + "/user/auth", json=auth_data, headers=headers)
+        auth_resp = auth_r.json()
+
+        access_token = auth_resp["access_token"]
+
+        get_headers = {"Authorization": "Bearer " + str(access_token) ,"client_id": self.client_id, "client_secret": self.client_secret, "Content-Type": "application/json"}
+        get_r = requests.get(self.base_url + "/user", headers=get_headers)
+        get_resp = get_r.json()
+
+        self.assertEqual(get_resp["spendingLimit"], 0, "Spending limit should be 0")
+        self.assertEqual(get_resp["username"], "UserGetTester", "Name should be UserGetTester")
+
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(exit=False)
+    print("Cleaning up tests.......")
+    post_data = {"passphrase": "TestingRocks!"}
+    cleanup = requests.post("http://localhost:5000/cleanup_tests", json=post_data)
+    print("Tests all cleaned up!")
+
 
 
 
